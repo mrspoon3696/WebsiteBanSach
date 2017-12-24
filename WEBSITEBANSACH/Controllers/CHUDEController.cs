c1a48f6b63a956fa46d17fe1bb87faeba1d6b0d6
nhucuong0807 <nhucuong0807@gmail.com>
2017-12-24 19:26:13 +0700

    WEB-35 chinh lai code 2 file

--- WEBSITEBANSACH/Controllers/CHUDEController.cs
+++ WEBSITEBANSACH/Controllers/CHUDEController.cs
@@ -8,7 +8,7 @@ namespace WEBSITEBANSACH.Controllers
 {
     public class CHUDEController : Controller
     {
-        //
+        //123
         // GET: /CHUDE/
         //xong roi nha tui sua lai ma chi tiet voi ma sach la int het nha uk  nên code luu ý y chang thầy
         //bam vo xem gio hang de xem gio 
