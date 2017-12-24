c1c87d6fe8d151f024f769abed6daceeacac7196
nhucuong0807 <nhucuong0807@gmail.com>
2017-12-24 19:42:52 +0700

    WEB-28 them controller NXB va 2 view NXBpartial va SPtheoNXB

---
+++ WEBSITEBANSACH/Controllers/NXBController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBSITEBANSACH.Models;
+
+namespace WEBSITEBANSACH.Controllers
+{
+    public class NXBController : Controller
+    {
+        //
+        // GET: /NXB/
+
+        //
+        // GET: /NXB/
+        SACHEntities13 db = new SACHEntities13();
+        public PartialViewResult NXBpartial()
+        {
+            var NXB = db.NHAXUATBANs.ToList();
+            return PartialView(NXB);
+        }
+        public ActionResult SPTHEONXB(int id)
+        {
+            var xb = from s in db.CHITIETSACHes where s.MANXB == id select s;
+            return View(xb);
+        }
+	}
+}
