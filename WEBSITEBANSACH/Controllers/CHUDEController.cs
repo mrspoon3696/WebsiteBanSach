77dce7a8540bb195492f9f8eec013634e7e42548
nhu cuong <nhucuong0807@gmail.com>
2017-12-24 19:15:02 +0700

    WEB-34 commit controller CHUDE va view chudepartical

---
+++ WEBSITEBANSACH/Controllers/CHUDEController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBSITEBANSACH.Models;
+namespace WEBSITEBANSACH.Controllers
+{
+    public class CHUDEController : Controller
+    {
+        //
+        // GET: /CHUDE/
+        //xong roi nha tui sua lai ma chi tiet voi ma sach la int het nha uk  nên code luu ý y chang thầy
+        //bam vo xem gio hang de xem gio 
+
+        SACHEntities13 db = new SACHEntities13();
+        public PartialViewResult CHUDEpartial()
+        {
+            var theloai = db.CHUDEs.ToList();
+            return PartialView(theloai);
+        }
+        public ActionResult SPTHEOCHUDE(int id)
+        {
+            var sach = from s in db.CHITIETSACHes where  s.MACHUDE == id select s;
+            return View(sach);
+        }
+	}
+}
