de83e89264fc853652bc90db41b3dd4a044c3627
quoc cuong <maitoquoccuong@gmail.com>
2017-12-24 18:55:40 +0700

    WEB-33 commit controller sachmoi

---
+++ WEBSITEBANSACH/Controllers/SachmoiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBSITEBANSACH.Models;
+namespace WEBSITEBANSACH.Controllers
+{
+    public class SachmoiController : Controller
+    {
+        //
+        // GET: /Sachmoi/
+        SACHEntities13 db = new SACHEntities13();
+        public PartialViewResult SACHMOIpartial()
+        {
+            var sachmoi = db.CHITIETSACHes.Take(4).ToList();
+            return PartialView(sachmoi);
+        }
+        public ActionResult chitiet(int id)
+        {
+            var chitiet = from s in db.CHITIETSACHes
+                          where s.MaCTS == id
+                          select s;
+            return View(chitiet.Single());
+        }
+	}
+}
